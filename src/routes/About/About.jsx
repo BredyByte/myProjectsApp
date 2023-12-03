436180363313efa8ed52872067194e04c90ec75c
Davyd Bredykhin <81176650+BredyByte@users.noreply.github.com>
2023-12-03 23:06:46 +0100

    style: About section text is changed

--- src/routes/About/About.jsx
+++ src/routes/About/About.jsx
@@ -6,32 +6,34 @@ const About = () => {
       className="About MainItem"
     >
       <div className="container" style={{ padding: "20px" }}>
-        <h2 className="title" style={{ textAlign: "center" }}>Hi there!</h2>
+        <h2 className="title" style={{ textAlign: "center" }}>Welcome on my Web Page!!</h2>
         <br/>
         <div>
-          First of all, I want to say <b>thank you</b> for visiting this application.
-          I made it  for a single purpose - to introduce you my person and projects that i've made.
+          First of all, I want to say <b>thank you</b> for stopping by!!!
+          My name is David, and this platform is my way of sharing a glimpse into who I am and the projects I've undertaken.
         </div>
-        <h2 style={{ textDecoration: "underline" }}><Link to="/projects">My Projects</Link></h2>
+        <h2 style={{ textDecoration: "underline" }}><Link to="/projects">Projects Page</Link></h2>
         <div>
-          I'm David, Frontend Developer with good experience in  ReactJS.
-          I play board games, do crossfit, hunt for new information, and i just adore programming!
-          So, if you have any suggestions to me, questions, well,
-          or just want to chat - write me an
-          email <b><a style={{ textDecoration: "underline" }} href="mailto:correodemimobil@gmail.com">correodemimobil@gmail.com</a></b> or
-          you'll be able to use any social net attached below.
+          I am David, a seasoned Frontend Developer with a strong proficiency in ReactJS. 
+          Apart from crafting code, I am an avid board gamer, a CrossFit enthusiast, 
+          and a perpetual learner always on the lookout for new information.
+
+          Feel free to explore my projects and, if you have any suggestions, 
+          questions, or just want to chat, don't hesitate to reach out via email at
+          <b><a style={{ textDecoration: "underline" }} href="mailto:correodemimobil@gmail.com">correodemimobil@gmail.com</a></b>. 
+          Alternatively, you can connect with me through the social networks listed below.
         </div>
         <br/>
         <div>
-          Oh, btw, i speak 4 languages:
+          Oh, by the way, I'm multilingual, fluent in four languages:
           <ul>
             <li>English</li>
             <li>Spanish</li>
             <li>Russian</li>
             <li>Ukrainian</li>
           </ul>
-          <p>So you can choose any one from this list.</p>
-          <p>See you later, internet traveler😏.</p>
+          <p>Choose any language from the list that suits you best.</p>
+          <p>Looking forward to connecting with you, fellow internet traveler! 😏</p>
         </div>
         <br/>
       </div>
